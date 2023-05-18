cd367d4890eae26f22bc27527ca25b265c1a9121
lena test <popovalena.i@yandex.ru>
2023-05-18 18:47:44 +0300

    task 4

--- task_4/solution.js
+++ task_4/solution.js
@@ -2,8 +2,21 @@
 
 function parseProducts(json) {
     // Аргументом функции является JSON
-
-    // Преобразуйте строку json, переданную как аргумент функции,
+    
+        let data = JSON.parse(json);
+        let products = data.products;
+        return products;
+        }
+        
+        function renderProductsCards(json) {
+        clearProducts();
+        let products = parseProducts(json);
+        let length = products.length;
+        for (let i = 0; i < length; i += 1) {
+        addProduct(products[i]);
+        }
+        
+            // Преобразуйте строку json, переданную как аргумент функции,
     // в объект с помощью функции JSON.parse(json)
     // и запишите в переменную data
     // Верните как результат функции свойство products объекта data
